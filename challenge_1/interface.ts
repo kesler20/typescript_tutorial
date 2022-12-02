3bb57ef5bed1b0ad44dab5a8da79797fb25bb316
kesler20 <uchekesla@gmail.com>
2022-12-02 11:42:03 +0000

    code: completed challenge 1 at the first try⭐

--- challenge_1/interface.ts
+++ challenge_1/interface.ts
@@ -1,13 +1,10 @@
-
-
-
 export interface House {
   name: string;
   planets: Array<string> | string;
 }
 
-export interface HouseWithID {
-  name: string;
-  planets: Array<string> | string;
+export interface HouseWithID extends House {
   id: number;
 }
+
+export type filterHouse = (house: House) => boolean;
--- challenge_1/main.ts
+++ challenge_1/main.ts
@@ -1,29 +1,35 @@
-import { House, HouseWithID } from "./interface";
+import { filterHouse, House, HouseWithID } from "./interface";
 import houses from "./houses.json";
 
+/**
+ * takes the houses of the user as a string or json and returns the house with id
+ * @param houses
+ * @param filterHouse
+ * @returns
+ */
 const findHouses = (
   houses: string | House[],
-  filterHouse: (house: House) => boolean
+  filterHouse?: filterHouse
 ): HouseWithID => {
   let housesWithID: Array<HouseWithID> = [];
   if (typeof houses === "string") {
     const parsedHouses: Array<House> = JSON.parse(houses);
     parsedHouses.forEach((house, id) => {
-      const { name, planets } = house;
-      housesWithID.push({ name, planets, id });
+      housesWithID.push({ ...house, id });
     });
   } else {
     houses.forEach((house, id) => {
-      const { name, planets } = house;
-      housesWithID.push({ name, planets, id });
+      housesWithID.push({ ...house, id });
     });
   }
-  return housesWithID.filter((houseWithID) => filterHouse(houseWithID))[0];
+  if (filterHouse) {
+    return housesWithID.filter((houseWithID) => filterHouse(houseWithID))[0];
+  } else {
+    return housesWithID[0];
+  }
 };
 
 console.log(
   findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
 );
-console.log(
-  findHouses(houses, ({ name }) => name === "Harkonnen")
-);
+console.log(findHouses(houses, ({ name }) => name === "Harkonnen"));
